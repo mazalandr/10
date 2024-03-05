9beb97a66ffccf28b37caa8e4e7f7a69d07b266c
mazalandr <156829875+mazalandr@users.noreply.github.com>
2024-03-05 19:14:24 +0300

    Update 12.js

--- 12.js
+++ 12.js
@@ -2,14 +2,12 @@
 // 1) Найти в коде список ul и добавить ему класс “list”.
 // 2) На li через один (начиная с самого первого) установить класс “item”
 // 3) На все ссылки в примере установить класс “custom-link”
-  let ul = document.getElementsByTagName('ul')
-for(i=0;i<ul.length;i=+2){
-  let ull = ul[i]
-  ull.setAttribute('class','item')
+  let ul = document.querySelectorAll('ul')
+  ul.forEach(element => { element.classList.add('list'); })
+  let li = document.querySelectorAll('li')
+for(i=0;i<li.length;i=+2){
+  li[i].classList.add('item')
 }
 
-let aaa = document.getElementsByTagName('a')
-for(i=0;i<aaa.length;i=+2){
-    let aa = aaa[i]
-    aa.setAttribute('class','custom-link')
-  }
+let aaa = document.querySelectorAll('a')
+aaa.forEach(element => { element.classList.add('custom-link'); })
